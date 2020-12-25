1003ba65bf6c6e205dc15b031165b1fe6b85db95
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2020-12-25 15:14:58 +0200

    fix some bugs to given test case  added complete assembling
    TODO:
    check that output is correct

--- assembler.py
+++ assembler.py
@@ -1,4 +1,4 @@
-import os
+import numpy as np
 import json
 
 instructions_path = "rules/instructions.json"
@@ -44,16 +44,25 @@ SYMBOL_TABLE = {}
 LABEL_TABLE = {}
 
 
+def bindigits(n, bits):
+    s = bin(n & int("1"*bits, 2))[2:]
+    return ("{0:0>%s}" % (bits)).format(s)
+
+
 def checkAddressingMode(word, isDe):
     global SYMBOL_TABLE
     try:
+
+        word = word.replace(" ", "").replace("\n", "").strip()
+        #print("word "+word)
+        #print("has "+SYMBOL_TABLE.has_key(word))
         if word in REGISTERS:
             return (0 + isDe*6)
-        if word[0] == '(' and word[-2] == ')' and word[-1] == '+':
+        if len(word) > 3 and word[0] == '(' and word[-2] == ')' and word[-1] == '+':
             return (1+isDe*6)
-        if word[0] == '-' and word[1] == '(' and word[-1] == ')':
+        if len(word) > 3 and word[0] == '-' and word[1] == '(' and word[-1] == ')':
             return (2+isDe*6)
-        if word[-4] == '(' and word[-1] == ')' and word[2:4] in REGISTERS:
+        if len(word) > 3 and word[-4] == '(' and word[-1] == ')' and word[2:4] in REGISTERS:
             return (3+isDe*6)
         if word[0] == "#":
             return (4+isDe*6)
@@ -64,6 +73,7 @@ def checkAddressingMode(word, isDe):
             return checkAddressingMode(newWord, 1)
         return -1
     except:
+        # print("exc")
         return -1
 
 
@@ -104,13 +114,13 @@ def AddExtraInstruction(i, line, mode):
     elif mode == 9:
         instruction += line[i+1][1:-4]
     elif mode == 4:
-        instruction += line[i+1][1:]
+        instruction += str(bindigits(int(line[i+1][1:], 10), 16))
     elif mode == 10:
         instruction += line[i+1][2:]
     elif mode == 5:
-        instruction += SYMBOL_TABLE[line[i+1]]
+        instruction += line[i+1]
     elif mode == 11:
-        instruction += SYMBOL_TABLE[line[i+1][1:]]
+        instruction += line[i+1][1:]
     return instruction
 
 
@@ -136,7 +146,8 @@ for line in file:
             word = word.strip().lower()
 
             if word == "define":
-                SYMBOL_TABLE[line[i+1]] = line[i+2]
+                SYMBOL_TABLE[line[i+1].strip().lower().replace(" ", "").replace("\n", "")
+                             ] = line[i+2].strip().lower().replace(" ", "").replace("\n", "")
                 break
             if word[-1] == ":":
                 print(word[:-1])
@@ -146,8 +157,10 @@ for line in file:
     except:
         print("Error in Assembling")
 file.seek(0)
-# print(SYMBOL_TABLE)
+# print(SYMBOL_TABLE.keys())
 noOfWordsArr = []
+
+print(checkAddressingMode("n", 0))
 # pass 2
 for line in file:
     # print(line)
@@ -162,6 +175,7 @@ for line in file:
             i += 1
 
         line = line.split()
+        # print(line)
         i = 0
         while i < len(line):
             word = line[i]
@@ -193,7 +207,7 @@ for line in file:
                     noOfWords = 1
                     mode1 = checkAddressingMode(line[i+1], 0)
                     mode2 = checkAddressingMode(line[i+3], 0)
-                    # print("mode2 "+str(mode2), line[i+3])
+                    #print("mode2 "+str(mode2), "mode1 "+str(mode1))
                     if mode1 == -1 or mode2 == -1:
                         raise Exception("not valid")
                     instruction = TWO_OP[word].replace(" ", "")
@@ -218,18 +232,26 @@ for line in file:
             elif word in BRANCH:
                 instruction = BRANCH[word].replace(" ", "")
                 instruction += LABEL_TABLE[line[i+1]]
-                FileArr.append(instruction+"\n")
+                FileArr.append(instruction)
+                FileArr.append("\n")
                 noOfWordsArr.append(1)
                 break
             elif word in NO_OP:
-                pass
+                instruction = NO_OP[word].replace(" ", "")
+                FileArr.append(instruction)
+                FileArr.append("\n")
+                noOfWordsArr.append(1)
+                break
             elif word in JUMP:
                 pass
             elif i == 0 and word[-1] == ':':
                 # label
-                pass
+                i += 1
+                continue
             elif word == "define":
-                FileArr.append(SYMBOL_TABLE[line[1]]+"\n")
+                base = int(SYMBOL_TABLE[line[i+1]], 10)
+                value = f'{base:016b}'
+                FileArr.append(str(value)+"\n")
                 noOfWordsArr.append(1)
                 break
             else:
@@ -244,14 +266,17 @@ file.seek(0)
 currAddress = 0
 j = 0
 # pass 3
+print(len(noOfWordsArr))
 for line in file:
     try:
         line = line.lower()
         line = line.split()
+        if len(line) < 1 or line[0][0] == ";":
+            continue
         # print(line)
         if line[0][-1] == ":":
             LABEL_TABLE[line[0][0:-1]] = str(currAddress)
-            if len(line) == 1:
+            if len(line) == 1 or line[1][0] == ";":
                 continue
         elif line[0] == "define":
             SYMBOL_TABLE[line[1]] = str(currAddress)
@@ -260,18 +285,37 @@ for line in file:
         j += 1
 
     except:
-        print("Error in pass 3 assembline")
+        print("Error in pass 3 assembling")
 # print(LABEL_TABLE, SYMBOL_TABLE, FileArr)
-print(LABEL_TABLE)
+print(LABEL_TABLE, FileArr)
 for i in range(len(FileArr)):
     x = FileArr[i][8:].replace("\n", "")
     if x in LABEL_TABLE:
         base = int(LABEL_TABLE[x], 10)
         #print(base, i)
         address = base-(i//2)
-        address = f'{address:08b}'
+        #address = f'{address:08b}'
+        # print(address)
+        address = bindigits(address, 8)
         #print("address ", address)
-        FileArr[i] = FileArr[i][:8] + str(address)+"\n"
+        FileArr[i] = FileArr[i][:8] + str(address)  # +"\n"
     elif FileArr[i] in SYMBOL_TABLE:
-        FileArr[i] = SYMBOL_TABLE[FileArr[i]]
+        base = int(SYMBOL_TABLE[FileArr[i]], 10)
+        # print(base)
+        address = base-(i//2)-1
+        #address = f'{address:016b}'
+        # print(address)
+        address = bindigits(address, 16)
+        FileArr[i] = str(address)
+
+INSTRUCTIONS = np.array([], dtype=np.int16)
+for i in range(len(FileArr)):
+    if FileArr[i] == "\n" or FileArr[i] == "":
+        continue
+    try:
+        x = int(FileArr[i].replace("\n", ""), 2)
+        np.append(INSTRUCTIONS, x)
+    except:
+        continue
+np.save("assembelled", INSTRUCTIONS)
 output_file.writelines(FileArr)
